6568a4376fc2f015fdd9c568a8bae0dd9ab66199
Jubilee Marshall <jam1703@nyu.edu>
2024-10-22 14:05:12 -0400

    homework

--- sketch.js
+++ sketch.js
@@ -1,6 +1,33 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let data = [];
+function preload() {
+  data = loadJSON("Beijing-PM2.5.json");
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 255, 255);
+
+  //loading the JSON file as an array rather than an object, so we can get its length
+  data = Object.values(data);
+
+  for (let idx = 0; idx <data.length; idx++) {
+    let aSample = data[idx];
+    let tempColor = map(aSample.temperature, -10, 20, 0, 255);
+    let ellipseX = map(idx, 0, data.length, 0, width);
+    let ellipseY=map(idx, 0, data.length, 0, height);
+    let ellipseD = map(aSample["pm2.5"], 0, 900, 1, 500);
+
+    //color conveys temperature and size of ellipse conveys amount of pollution
+
+    fill(tempColor, 0, 0, 100);
+    stroke(tempColor, 0, 0, 100);
+    ellipse(ellipseX, ellipseY, ellipseD);
+
+    print(data.length)
+  }
 }
+
+
+
+
+  
